cdab24cdf459b25a582d5a252daa06874626d2e4
matherg <72321618+matherg@users.noreply.github.com>
2021-11-16 22:14:47 -0500

    fix 6 11-13

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 import numpy as np
-import cv2 as cv
+import cv2
 from matplotlib import pyplot as plt
-img = cv.imread('n7.jpg')
 
+img = cv2.imread('n13.jpeg')
 
-#dst = cv.fastNlMeansDenoisingColored(img,None,20,20,7,21)
-dst = cv.fastNlMeansDenoising(img,None,10,7,21)
-
+dst = cv2.fastNlMeansDenoisingColored(img,None,5,5,7,21)
+#dst = cv2.fastNlMeansDenoising(img,None,12,7,21)
 plt.subplot(121),plt.imshow(img)
 plt.subplot(122),plt.imshow(dst)
 plt.show()
 
-
-*cv.imwrite('n5.jpg', dst)
+cv2.imwrite('n13.jpeg', dst);
